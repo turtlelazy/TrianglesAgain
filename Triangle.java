06805434d968f2d36245f5d2ba79c94938a07e89
Ishraq <ishraqmahid@gmail.com>
2020-10-31 11:37:43 -0400

    created 1 of 2 constructor methods

--- Triangle.java
+++ Triangle.java
@@ -1,4 +1,15 @@
 public class Triangle{
 	private Point v1,v2,v3;
+	
+	public Triangle(double x1, double y1,double x2, double y2,double x3, double y3){
+		v1 = Point(x1,y1);
+		v2 = Point(x2,y2);
+		v3 = Point(x3,y3);
+
+	}
+	
+	
+	
+	
 
 }
